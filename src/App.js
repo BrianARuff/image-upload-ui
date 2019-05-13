ee101d774b0bd168031fcbf41687ecb9e1969241
Brian Ruff <thelonewolf198927@gmail.com>
2019-05-12 23:23:21 -0400

    image upload app ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    images: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://www.localhost:9000/")
+      .then(res => {
+        this.setState({ images: res.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  upload = element => {
+    var file = element.target.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      console.log("RESULT", reader.result);
+      axios
+        .post("http://www.localhost:9000/", { image: reader.result })
+        .catch(err => console.log(err));
+    };
+    reader.readAsDataURL(file);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <label>Upload</label>
+        <input onChange={this.upload} type="file" />
+        <div>
+          {this.state.images.map(item => {
+            return <img width="500px" height="500px" src={item.image} alt="item" />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
